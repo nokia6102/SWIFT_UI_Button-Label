ec2b157184f94b7dd10f6b1258731aece64b1aca
nokia6102 <nokia6102@gmail.com>
2015-12-26 16:33:04 +0800

    Initial Commit

---
+++ button/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  button
+//
+//  Created by Leon Cheng on 2015/12/26.
+//  Copyright (c) 2015年 Leon Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
